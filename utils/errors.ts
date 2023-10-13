cc13c83761ea026a0e074c9dd60ec43f5fe4645d
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-10-13 16:28:16 +0200

    Add types

--- utils/errors.ts
+++ utils/errors.ts
@@ -5,7 +5,7 @@ export class ValidationError extends Error {}
 export const handleError = (err: Error, req: Request, res: Response): void => {
   console.error(err);
 
-  res.status(err instanceof ValidationError ? 400 : 500).render("error", {
+  res.status(err instanceof ValidationError ? 400 : 500).json({
     message:
       err instanceof ValidationError
         ? err.message
